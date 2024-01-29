2a2a91e4b4415a67d66bee88896fef660cafaa6c
csprashant <prashant.puse@innodeed.com>
2024-01-29 18:31:39 +0530

    added exception handling

--- src/main/java/com/inno/dto/Student.java
+++ src/main/java/com/inno/dto/Student.java
@@ -4,15 +4,19 @@ public class Student {
 
 	private String name;
 	private Double percent;
+	
 	public String getName() {
 		return name;
 	}
+	
 	public void setName(String name) {
 		this.name = name;
 	}
+	
 	public Double getPercent() {
 		return percent;
 	}
+	
 	public void setPercent(Double percent) {
 		this.percent = percent;
 	}
--- src/main/java/com/inno/service/TemplateService.java
+++ src/main/java/com/inno/service/TemplateService.java
@@ -2,6 +2,8 @@ package com.inno.service;
 
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
@@ -13,32 +15,43 @@ import com.inno.dto.Student;
 
 @Service
 public class TemplateService {
-	@Autowired	private JdbcTemplate jdbcTemplate;
+	
+	@Autowired private JdbcTemplate jdbcTemplate;
 
+	Logger LOGGER = LoggerFactory.getLogger(TemplateService.class);
+	
 	public String getNameById(Integer id) {
-		SimpleJdbcCall simpleJdbcCall = new SimpleJdbcCall(jdbcTemplate);
-		SqlParameterSource in = new MapSqlParameterSource().addValue("eid", id);
-		Map<String, Object> out = simpleJdbcCall.withProcedureName("getInfo").execute(in);
-		if(!out.isEmpty()) {
-			return out.get("ENAME").toString();
+		try {
+			SimpleJdbcCall simpleJdbcCall = new SimpleJdbcCall(jdbcTemplate);
+			SqlParameterSource in = new MapSqlParameterSource().addValue("eid", id);
+			Map<String, Object> out = simpleJdbcCall.withProcedureName("getInfo").execute(in);
+			if(!out.isEmpty()) {
+				return out.get("ENAME").toString();
+			}
+			return null;
 		}
-		return null;
+		catch(Exception e) {
+			LOGGER.error("Exception in calling procedure Exception : {} ",e.getMessage());
+			return null;
+		}
 	}
 	
 	public Student getStudentDetailsById(Integer id) {
-		SimpleJdbcCall simpleJdbcCall = new SimpleJdbcCall(jdbcTemplate);
-		SqlParameterSource in = new MapSqlParameterSource().addValue("EID", id);
-		Map<String, Object> out = simpleJdbcCall.withProcedureName("GET_DETAILS").execute(in);
-		if(!out.isEmpty()) {
-			Student student = new Student();
-			student.setName(out.get("SNAME").toString()); 
-			student.setPercent(Double.parseDouble(out.get("SPER").toString()));
-			return student;		
+		try {
+			SimpleJdbcCall simpleJdbcCall = new SimpleJdbcCall(jdbcTemplate);
+			SqlParameterSource in = new MapSqlParameterSource().addValue("EID", id);
+			Map<String, Object> out = simpleJdbcCall.withProcedureName("GET_DETAILS").execute(in);
+			if(!out.isEmpty()) {
+				Student student = new Student();
+				student.setName(out.get("SNAME").toString()); 
+				student.setPercent(Double.parseDouble(out.get("SPER").toString()));
+				return student;		
+			}
+			return null;
 		}
-		return null;
-				
+		catch(Exception e) {
+			LOGGER.error("Exception in calling procedure Exception : {} ",e.getMessage());
+			return null;
+		}
 	}
-	
-	
-	
 }
